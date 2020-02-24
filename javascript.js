29e3f0ea51101cb6cb7ca2603506bf7cdbea725e
Austin T <austin.taranto@gmail.com>
2020-02-24 17:59:26 -0500

    timecode split and map parsefloat

--- javascript.js
+++ javascript.js
@@ -3,4 +3,8 @@ const timeNodes = Array.from (document.querySelectorAll("[data-time]"));
 
 // console.log(timeNodes);
 
-const seconds = timeNodes.map(node => node.dataset.time);
+const seconds = timeNodes.map(node => node.dataset.time).map(timeCode => {
+    const [mins, secs] = timeCode.split(":").map(parseFloat);
+    return (mins * 60) + secs;
+    console.log(mins, secs);
+})
